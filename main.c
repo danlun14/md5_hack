a5c1b52d299cb39c858727d2ba4cadb633406f23
Данила Лунев <Danila@localhost.localdomain>
2021-12-09 21:13:15 +0700

    Add processing of command line argument <md5_hash>

--- main.c
+++ main.c
@@ -1,19 +1,78 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void help_msg()
+void helpMsg()
 {
     printf("\nUsage command line arguments (without \"):\n");
-    printf("\t./md5_hack <md5_hash> <alphabet> <string_lenght>\n\n");
+    printf("\t./md5_hack <md5_hash> <alphabet> <string_lenght>\n");
+    printf("\t<md5_hash> contains 32 symbols [a-f0-9]\n\n");
 }
 
-int main(int argc, char *argv[])
+// hash string to hex(int)
+uint8_t* hashStoh(char* source)
+{
+    int source_len = strlen(source);
+    printf("%d\n", source_len);
+    uint8_t* hash;
+    if (source_len == 32)
+    {
+        hash = malloc(16);
+        for (int i = 0; i < 32; i++)
+        {
+            int value = 0;
+            if (*(source + i) >= '0' && *(source + i) <= '9')
+            {
+                value = *(source + i) - '0';
+            }
+            else if ((*(source + i) >= 'a' && *(source + i) <= 'f'))
+            {
+                value = 10 + *(source + i) - 'a';
+            }
+            else
+            {
+                free(hash);
+                exit(0);
+            }
+
+            int id = i / 2;
+            if (i % 2 == 0)
+            {
+                hash[id] = value;
+            }
+            else
+            {
+                hash[id] = hash[id] << 4;
+                hash[id] += value;
+            }
+        }
+    }
+    else
+    {
+        exit(0);
+    }
+    return hash;
+}
+
+void print_hash(uint8_t* p)
+{
+    for (unsigned int i = 0; i < 16; ++i)
+    {
+        printf("%02x", p[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
 {
     if (argc != 4)
     {
-        help_msg();
+        helpMsg();
         return 0;
     }
     int string_size = atoi(argv[3]);
-    const char *alphabet = argv[2];
+    const char* alphabet = argv[2];
+    uint8_t* hash = hashStoh(argv[1]);
+    print_hash(hash);
 }
